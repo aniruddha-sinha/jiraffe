3703f04ebe9094537003eac830111924ba5f6d68
Aniruddha Sinha <asinha0493@gmail.com>
2026-04-29 23:36:41 +0530

    added JIRA level config such as URL

---
+++ config/config.go
@@ -0,0 +1,11 @@
+package config
+
+type Config struct {
+	AuthURL     string
+	TokenURL    string
+	APIBaseURL  string
+	CallbackURL string
+}
+
+//! var cfg *Config
+//! var embeddedConfig embed.FS
