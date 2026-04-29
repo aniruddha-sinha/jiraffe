e6fa71a04e8d2bb489d254b6a69ac197b27994c8
Aniruddha Sinha <asinha0493@gmail.com>
2026-04-29 23:08:45 +0530

    framed the initial command structure

---
+++ cmd/jira.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var newJiraCmd = &cobra.Command{
+	Use:     "jira",
+	Aliases: []string{"j"},
+	Short:   "subcommand for JIRA",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println("jira called")
+		return nil
+	},
+}
+
+var newAuthCmd = &cobra.Command{
+	Use:     "auth",
+	Aliases: []string{"a"},
+	Short:   "subcommand for auth",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println("auth called")
+		return nil
+	},
+}
+
+func init() {
+	// wiring the commands
+	rootCmd.AddCommand(newJiraCmd)
+	newJiraCmd.AddCommand(newAuthCmd)
+}
